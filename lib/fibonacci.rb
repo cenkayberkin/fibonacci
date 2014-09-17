adb933c5cd85f9c3d45c95d2fd9885a7db9c4231
Cenk Ayberkin <cenkayberkin@gmail.com>
2014-09-17 13:43:30 -0700

    Simple and slow version of finding fibonacci number

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,5 +1,17 @@
+#Simple slow version of finding fibonacci numbers.
 class Fibonacci
-  def self.calculate_Nth_Fib_Num(num)
-    1
+  def self.find_fibNum(num)
+    if num == 2
+      1
+    elsif num == 1
+      0
+    else
+      find_fibNum(num - 1) + find_fibNum(num - 2)
+    end
   end
 end
+
+# (1..10).to_a.each do |n|
+#   p Fibonacci.fibNums(n)
+# end
+
