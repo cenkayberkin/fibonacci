dd05fbbe899edb62dd17911ed3fcffda39dc92dd
Cenk Ayberkin <cenkayberkin@gmail.com>
2014-09-17 14:18:13 -0700

    My ultra fast fibonacci solution added

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,17 +1,41 @@
-#Simple slow version of finding fibonacci numbers.
+# My ultra fast version of Fib onacci
 class Fibonacci
+  @@result_hash = {}
   def self.find_fibNum(num)
-    if num == 2
-      1
-    elsif num == 1
-      0
+    if @@result_hash.key? num
+      # puts "found it no calc #{num}"
+      @@result_hash[num]
+
     else
-      find_fibNum(num - 1) + find_fibNum(num - 2)
+      if num == 0
+        0
+      elsif num == 1
+        1
+      else
+        num_fib_value = find_fibNum(num - 1) + find_fibNum(num - 2)
+        @@result_hash[num] = num_fib_value
+        num_fib_value
+      end
     end
   end
+
+  # def self.find_fibNum2(num)
+  #   if num == 0
+  #     0
+  #   elsif num == 1
+  #     1
+  #   else
+  #     find_fibNum2(num - 1) + find_fibNum2(num - 2)
+  #   end
+  # end
 end
 
-# (1..10).to_a.each do |n|
-#   p Fibonacci.fibNums(n)
+# p Fibonacci.find_fibNum2(5)
+
+# (1..10_000).to_a.each do |n|
+#   p Fibonacci.find_fibNum(n)
 # end
 
+# (1..10).to_a.each do |n|
+#   p Fibonacci.find_fibNum(n)
+# end
--- spec/fibonacci_spec.rb
+++ spec/fibonacci_spec.rb
@@ -4,23 +4,23 @@ require "minitest/autorun"
 require "fibonacci"
 
 describe "testing.." do
-  it "matches 10th fib num" do
-    num = Fibonacci.find_fibNum 10
+  it "matches 9th fib num" do
+    num = Fibonacci.find_fibNum 9
     assert_equal 34,num
   end
 
-  it "matches 8th fib num" do
-    num = Fibonacci.find_fibNum 8
+  it "matches 7th fib num" do
+    num = Fibonacci.find_fibNum 7
     assert_equal 13,num
   end
 
-  it "should match 5th num" do
-    num = Fibonacci.find_fibNum 5
+  it "should match 4th num" do
+    num = Fibonacci.find_fibNum 4
     assert_equal 3,num
   end
 
-  it "should match 9th num" do
-    num = Fibonacci.find_fibNum 9
+  it "should match 8th num" do
+    num = Fibonacci.find_fibNum 8
     assert_equal 21,num
   end
 end
